8128b59abddebb4892e2d9a8f8dbfadcd592075e
ukroy0 <107434798+ukroy0@users.noreply.github.com>
2022-06-14 01:21:42 +0530

    Add files via upload

---
+++ RackBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc2021;
+
+/**
+ *
+ * @author ujjawal kumar
+ */
+public class RackBean 
+{
+    private int rackno;
+    private String subject;
+
+    /**
+     * @return the rackno
+     */
+    public int getRackno() {
+        return rackno;
+    }
+
+    /**
+     * @param rackno the rackno to set
+     */
+    public void setRackno(int rackno) {
+        this.rackno = rackno;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+}
